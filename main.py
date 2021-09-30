764d5a697e93babdeea2a12510230eb233adf96a
mahdi13761376 <mahdi1376ghanbari"gmail.com>
2021-09-30 11:39:38 +0100

    final version

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ import send_request
 import time
 import cv2
 
+
 device_id = 2
 while True:
     print('press any to ring the bell')
     ring = input()
     face = None
     while face is None:
-        time.sleep(0.5)
+        time.sleep(1)
         face = face_detection.get_faces()
-    send_request.send('http://192.168.188.68:8000/ring/',device_id,'picture.png')
+    print('face detected')
+    res = send_request.send_ring('http://192.168.188.68:8000/ring/',device_id,'picture.png')
+    print (res)
+    if res =='"open"':
+        print('door is opened')
+    else:
+       time.sleep(30)
+       res = send_request.send_change('http://192.168.188.68:8000/check/',device_id)
+       print (res)
--- send_request.py
+++ send_request.py
@@ -1,6 +1,7 @@
 import requests
 import base64
-def send(url, device_id, img_address ):
+
+def send_ring(url, device_id, img_address ):
     with open (img_address , 'rb') as img:
         ba_64_img = base64.b64encode(img.read())
         data = {'device_id':device_id,
@@ -8,5 +9,11 @@ def send(url, device_id, img_address ):
             }
         r = requests.post(url = url, data = data)
         res = r.text
-        print(res);
         return res;
+    
+def send_change(url, device_id):
+    data = {'device_id':device_id
+        }
+    r = requests.post(url = url, data = data)
+    res = r.text
+    return res;
